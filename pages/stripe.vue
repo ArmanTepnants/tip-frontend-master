66668193263bf1c6d8a5ae2757a9feb021fd18ca
ArmanTepnants <100276773+ArmanTepnants@users.noreply.github.com>
2024-02-14 15:21:53 +0400

    Update stripe.vue

--- pages/stripe.vue
+++ pages/stripe.vue
@@ -1,102 +1,3 @@
-<!--<template>-->
-<!--  <div>-->
-<!--    <div id="payment-request-button"></div>-->
-<!--  </div>-->
-<!--</template>-->
-
-<!--<script>-->
-<!--import {loadStripe} from '@stripe/stripe-js';-->
-
-<!--export default {-->
-
-<!--  data() {-->
-<!--    return {-->
-<!--      stripe: null,-->
-<!--      paymentRequest: null,-->
-<!--      prButton: null,-->
-<!--      amount: 3 * 100,-->
-<!--    };-->
-<!--  },-->
-
-<!--  async mounted() {-->
-<!--    this.stripe = await loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);-->
-<!--    this.setupPaymentRequest();-->
-<!--  },-->
-<!--  methods: {-->
-<!--    async setupPaymentRequest() {-->
-<!--      if (!this.stripe) {-->
-<!--        console.error('Stripe has not been initialized');-->
-<!--        return;-->
-<!--      }-->
-
-<!--      // Создание объекта paymentRequest-->
-<!--      this.paymentRequest = this.stripe.paymentRequest({-->
-<!--        country: 'ES',-->
-<!--        currency: 'eur',-->
-<!--        total: {-->
-<!--          label: 'Total',-->
-<!--          amount: this.amount, // Используйте payAmount напрямую-->
-<!--        },-->
-<!--      });-->
-
-<!--      // Проверка доступности Google Pay и Apple Pay-->
-<!--      const result = await this.paymentRequest.canMakePayment();-->
-<!--      console.log("Payment Method - " + JSON.stringify(result))-->
-<!--      if (result) {-->
-<!--        this.prButton = this.stripe.elements().create('paymentRequestButton', {-->
-<!--          paymentRequest: this.paymentRequest,-->
-<!--        });-->
-<!--        this.prButton.mount('#payment-request-button');-->
-<!--      } else {-->
-<!--        document.getElementById('payment-request-button').style.display = 'none';-->
-<!--      }-->
-
-<!--      this.paymentRequest.on('paymentmethod', async (event) => {-->
-<!--        try {-->
-<!--          // Отправка запроса для создания Payment Intent-->
-<!--          const res = await this.$axios.post('/create-payment-intent', {-->
-<!--            currency: "eur",-->
-<!--            amount: this.amount-->
-<!--          })-->
-<!--          const {clientSecret} = res.data-->
-
-<!--          console.log("Client Secret - " + clientSecret)-->
-
-<!--          if (!clientSecret) {-->
-<!--            throw new Error("Client secret is not available.");-->
-<!--          }-->
-
-<!--          // Подтверждение платежа с использованием Stripe-->
-<!--          const response = await this.stripe.confirmCardPayment(clientSecret, {-->
-<!--            payment_method: event.paymentMethod.id-->
-<!--          });-->
-
-<!--          if (response.error) {-->
-<!--            // Обработка ошибки при платеже-->
-<!--            // alert("Ошибка при платеже: " + response.error.message);-->
-<!--            console.error("Ошибка при платеже: ", response.error.message);-->
-<!--            event.complete('fail');-->
-<!--          } else {-->
-<!--            // Успешный платеж-->
-<!--            // alert('Payment successful');-->
-<!--            console.log('Payment successful');-->
-<!--            event.complete('success');-->
-<!--            this.$router.push("/payment/success");-->
-<!--          }-->
-<!--        } catch (error) {-->
-<!--          // Обработка исключений-->
-<!--          console.log("Ошибка " + error);-->
-<!--          // alert("Ошибка при платежe");-->
-<!--          event.complete('fail');-->
-<!--        }-->
-<!--      });-->
-<!--    },-->
-<!--  },-->
-<!--};-->
-<!--</script>-->
-
-
-<!--Раскоментировать этот код чтобы загрузить на сервер-->
 <template>
   <div>
     <div id="payment-request-button"></div>
@@ -104,18 +5,16 @@
 </template>
 
 <script>
-import { loadStripe } from '@stripe/stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
 export default {
-  props: {
-    payAmount: Number,
-  },
 
   data() {
     return {
       stripe: null,
       paymentRequest: null,
       prButton: null,
+      amount: 3 * 100,
     };
   },
 
@@ -123,16 +22,12 @@ export default {
     this.stripe = await loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
     this.setupPaymentRequest();
   },
-
   methods: {
     async setupPaymentRequest() {
-      console.log(this.payAmount)
       if (!this.stripe) {
         console.error('Stripe has not been initialized');
         return;
       }
-
-      console.log('Using payAmount for payment request:', this.payAmount);
 
       // Создание объекта paymentRequest
       this.paymentRequest = this.stripe.paymentRequest({
@@ -140,13 +35,13 @@ export default {
         currency: 'eur',
         total: {
           label: 'Total',
-          amount: this.payAmount * 100, // Использование payAmount напрямую
+          amount: this.amount, // Используйте payAmount напрямую
         },
       });
 
       // Проверка доступности Google Pay и Apple Pay
       const result = await this.paymentRequest.canMakePayment();
-      console.log('Payment Method:', JSON.stringify(result));
+      console.log("Payment Method - " + JSON.stringify(result))
       if (result) {
         this.prButton = this.stripe.elements().create('paymentRequestButton', {
           paymentRequest: this.paymentRequest,
@@ -160,32 +55,38 @@ export default {
         try {
           // Отправка запроса для создания Payment Intent
           const res = await this.$axios.post('/create-payment-intent', {
-            currency: 'eur',
-            amount: this.payAmount * 100, // Использование payAmount напрямую
-          });
-          const { clientSecret } = res.data;
+            currency: "eur",
+            amount: this.amount
+          })
+          const {clientSecret} = res.data
 
-          console.log('Client Secret:', clientSecret);
+          console.log("Client Secret - " + clientSecret)
 
           if (!clientSecret) {
-            throw new Error('Client secret is not available.');
+            throw new Error("Client secret is not available.");
           }
 
           // Подтверждение платежа с использованием Stripe
           const response = await this.stripe.confirmCardPayment(clientSecret, {
-            payment_method: event.paymentMethod.id,
+            payment_method: event.paymentMethod.id
           });
 
           if (response.error) {
-            console.error('Ошибка при платеже:', response.error.message);
+            // Обработка ошибки при платеже
+            // alert("Ошибка при платеже: " + response.error.message);
+            console.error("Ошибка при платеже: ", response.error.message);
             event.complete('fail');
           } else {
+            // Успешный платеж
+            // alert('Payment successful');
             console.log('Payment successful');
             event.complete('success');
-            this.$router.push('/payment/success');
+            this.$router.push("/payment/success");
           }
         } catch (error) {
-          console.log('Ошибка:', error);
+          // Обработка исключений
+          console.log("Ошибка " + error);
+          // alert("Ошибка при платежe");
           event.complete('fail');
         }
       });
